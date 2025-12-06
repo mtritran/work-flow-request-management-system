9c6eb66b0e318962745033887d7d0c3b6db2f519
mtritran <tranmanhtri5104@gmail.com>
2025-12-06 11:19:58 +0700

    Initial project

---
+++ src/main/java/com/mtritran/workflow_request_management_system/WorkflowRequestManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mtritran.workflow_request_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowRequestManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowRequestManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtritran/workflow_request_management_system/WorkflowRequestManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtritran.workflow_request_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowRequestManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
